5d7c610a6bee312cba5d1e1a601ac8c5d914427c
刘一帆 <yifanliu@farasisenergy.com.cn>
2021-05-20 14:40:54 +0800

    日志1

--- Map_ez.c
+++ Map_ez.c
@@ -4,7 +4,9 @@
 // signals auto re-mapping
 
 
-
+/** GUI的作用主要是便于选择将要替换的变量  提供可视化操作
+ * 
+*/
 
 int main
 {
@@ -12,3 +14,5 @@ int main
 
     return 0;
 }
+
+// 暂时决定用java实现。 因为GUI的实现会简单一些。  预计开发周期为15天。。
